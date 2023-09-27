a5df75b0fea8c9f7b945924cb9a2378821a2988c
Rustam Gilmutdinov <rukavina_sb@mail.ru>
2023-09-27 19:08:01 +0500

    now it seems good

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 
-// function towelSort(matrix) {
-//   return [].concat(...matrix)
-// }
+function towelSort(matrix) {
+  if (!matrix || matrix.length === 0) {
+    return []
+  }
+  return [].concat(...matrix.map((row, index) => index % 2 === 0 ? row : row.reverse() ) )
+}
 
 module.exports = function towelSort (matrix) {
-  return [].concat(...matrix);
-}
+  return [].concat(...matrix.map((row, index) => index % 2 === 0 ? row : row.reverse() ) )
+};
+
+//Should return empty array if no params passed

e47ea05462e5c2dd784616e20f61fa10f462c5cc
Rustam Gilmutdinov <91172406+rukavin1@users.noreply.github.com>
2024-08-20 16:43:52 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ module.exports = function towelSort (matrix) {
   if (matrix === undefined || !matrix || matrix.length === 0) {
     return []
   }
-  return [].concat(...matrix.map((row, index) => index % 2 === 0 ? row : row.reverse() ) )
+  let flatten = [];
+      for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+          flatten.push(matrix[i][j]);
+        }
+      } return flatten;
 };
 
 //Should return empty array if no params passed
+
